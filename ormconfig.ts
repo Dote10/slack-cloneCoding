9ddd9a5924930b0820ff4a3da7f3301a9dd94eff
Dote10 <75302063+Dote10@users.noreply.github.com>
2022-06-27 20:54:18 +0900

    exceptionfilter공부

--- ormconfig.ts
+++ ormconfig.ts
@@ -17,7 +17,7 @@ export const typeConfig:TypeOrmModuleOptions ={
   
   type: 'mysql',
   host: process.env.DB_HOST,
-  port: 3308,
+  port: 3306,
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Users } from './entities/Users';
+import bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class UsersService {
@@ -12,6 +14,22 @@ export class UsersService {
 
   async join(email: string, nickname: string, password: string) {
      const user = await this.usersRepository.findOne({where:{email}});
+
+     if(!email){
+      throw new HttpException('이메일이 없습니다.',400);
+     }
+
+     if(user){
+      throw new HttpException('이미 존재하는 사용자입니다.',400);
+     }
+
+     const hashedPassword = await bcrypt.hash(password,12);
+     await this.usersRepository.save({
+      email,
+      nickname,
+      password:hashedPassword
+     });
+
   }
 }
 
