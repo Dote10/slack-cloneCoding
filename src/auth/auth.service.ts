dabd2c99bcb2c5d3c242a5425c1a0b9c1497662b
Dote10 <zoqol@daum.net>
2022-09-27 22:00:29 +0900

    make payload and token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,14 @@ import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { LoginRequestDto } from '../users/dto/login.request.dto';
 import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(
+    private readonly usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
 
   async jwtLogIn(data: LoginRequestDto) {
     const { email, password } = data;
@@ -28,5 +32,14 @@ export class AuthService {
     }
 
     //* 유효성 검사가 끝났으면 프론트에 jwt를 반환해줘야 한다.
+    // 그럼 프론트엔드가 그 jwt를 받아서 안전한 곳에 저장한다.
+
+    //프론트에 jwt를 반환하려면 nestJwt에 존재하는 JwtService를 사용해야한다.
+
+    const payload = { email, sub: user.id };
+
+    return {
+      token: this.jwtService.sign(payload),
+    };
   }
 }
